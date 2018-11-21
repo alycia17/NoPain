1fdff20e50477831416d0afa2082595ff9b1d78b
aluno <kalebeking@gmail.com>
2018-11-21 15:08:45 -0200

    atualização do BD

---
+++ NoPain/Banco de dados No Pain.sql
@@ -0,0 +1,17 @@
+﻿-- Table: no_pain.endereco
+
+-- DROP TABLE no_pain.endereco;
+
+CREATE TABLE no_pain.endereco
+(
+  nm_rua character varying,
+  nm_bairro character varying,
+  nm_cidade character varying,
+  cd_endereco integer NOT NULL,
+  CONSTRAINT pk_farmacia PRIMARY KEY (cd_endereco)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE no_pain.endereco
+  OWNER TO postgres;
